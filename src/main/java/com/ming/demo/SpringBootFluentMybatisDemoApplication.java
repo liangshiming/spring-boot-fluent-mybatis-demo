efc5ceb6040857166a24e98c2762cae3dd45134f
liangshiming-ds <liangshiming-ds@gome.com.cn>
2022-05-20 11:08:00 +0800

    Initial commit

---
+++ src/main/java/com/ming/demo/SpringBootFluentMybatisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ming.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFluentMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFluentMybatisDemoApplication.class, args);
+	}
+
+}
